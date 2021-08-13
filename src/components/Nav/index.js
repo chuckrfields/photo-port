ea85976951d0f48883805d2a21314d8042401b0b
Chuck Fields <info@spacetechcorp.com>
2021-08-13 10:16:07 -0400

    Added tests for Nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,7 +5,7 @@ function Nav() {
     function categorySelected() {
         console.log("hello")
       }
-      
+
     const categories = [
         {
           name: "commercial",
@@ -23,14 +23,14 @@ function Nav() {
     return (
         <header>
             <h2>
-                <a href="/">
+                <a data-testid="link" href="/">
                 <span role="img" aria-label="camera"> 📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                 <li className="mx-2">
-                    <a href="#about">
+                    <a data-testid="about" href="#about">
                     About me
                     </a>
                 </li>
